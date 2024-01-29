dec489d2b66c9eb733e268935bd648678826338d
jomaway <jomaway@users.noreply.github.com>
2024-01-29 10:27:26 +0100

    Added gp timer oc example CMSIS version

--- CMSIS/nucleo-f401re_gp-timer-oc_CMSIS/src/main.c
+++ CMSIS/nucleo-f401re_gp-timer-oc_CMSIS/src/main.c
@@ -5,52 +5,57 @@
 /* Includes */
 #include <stm32f4xx.h>
 #include <stdbool.h>
-#include <gpio.h>
-#include <timer.h>
 
 /* Defines */
+#define LED_PORT GPIOA
+#define LED_PIN 6
+#define LED_PIN_ALTFUNC 2
+
 #define TIM3_PRESCALER 16000 - 1
 #define TIM3_AUTO_RELOAD_VALUE 1000
-#define TIM_CCMR1_OC1M_TOGGLE  (0b11UL << 4U)
+#define TIM_CCMR1_OC1M_TOGGLE (0b11UL << 4U)
 
 /* Function declaration */
 void timer3_init();
 
 /* global Variables */
 
-int main()
+void gpio_setup()
 {
-
-    gpio_enable_port_clock(GPIOA);
-    gpio_set_pin_mode(GPIOA, PIN_6, ALTFUNC);  // Set PA6 to alternate function
-    gpio_set_pin_af(GPIOA, PIN_6, AF2);  // AF-Modus: AF2 -> TIM3_CH1  (Table 9. Alternate function mapping)
-
-    timer3_init();
-    timer_enable(TIM3);
-
-    while(1)
-    {
-        // Do nothing
-    }
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    GPIOA->MODER |= GPIO_MODER_MODER6_0;
+    GPIOA->AFR[0] |= (LED_PIN_ALTFUNC << ((LED_PIN & 7) * 4));
+    // AF-Modus: AF2 -> TIM3_CH1  (Table 9. Alternate function mapping)
 }
 
-/* Funktions */
-void timer3_init()
+void timer_setup()
 {
     // Enable clock for TIM3 on APB1 Bus
-    timer_enable_bus_clock(TIM3);
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 
-    // Set Prescaler for TIM3 
-    timer_set_prescaler(TIM3, TIM3_PRESCALER);
+    // Set Prescaler for TIM3
+    TIM3->PSC = TIM3_PRESCALER;
 
     // Set Auto Relaod Register
-    timer_set_auto_reload_value(TIM3, TIM3_AUTO_RELOAD_VALUE);
+    TIM3->ARR = TIM3_AUTO_RELOAD_VALUE;
 
     // Set Toggle mode.
     TIM3->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1;
-    //timer_set_oc_mode(TIM3, TIM_CH1, CHN_TOGGLE);
 
     // Activate Channel 1 as output
     TIM3->CCER |= TIM_CCER_CC1E;
-    //timer_enable_oc_channel(TIM3, TIM_CH1);
+
+    // Enable timer
+    TIM3->CR1 |= TIM_CR1_CEN;
+}
+
+int main()
+{
+    gpio_setup();
+    timer_setup();
+
+    while (1)
+    {
+        // Do nothing
+    }
 }
