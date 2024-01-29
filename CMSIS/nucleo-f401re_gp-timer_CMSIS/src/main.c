7cd60b0a68a4c1353869a38e2b4b9905e32597f9
jomaway <jomaway@users.noreply.github.com>
2024-01-29 10:07:51 +0100

    Added gp timer example CMSIS version

--- CMSIS/nucleo-f401re_gp-timer_CMSIS/src/main.c
+++ CMSIS/nucleo-f401re_gp-timer_CMSIS/src/main.c
@@ -5,31 +5,50 @@
 /* Includes */
 #include <stm32f4xx.h>
 
-#include <stdbool.h>
-#include <gpio.h>
-
 /* Defines */
+#define LED_PORT GPIOA
+#define LED_PIN 5
+
 #define TIM3_PRESCALER 16000 - 1
 #define TIM3_AUTO_RELOAD_VALUE 1000 - 1
-#define TIM3_INTERUPT_ENABLE true
 
 /* Function declaration */
-void timer_init(TIM_TypeDef *timer, uint16_t prescaler, uint16_t reload_value, bool interrupt_enable);
-void timer_reset_uif(TIM_TypeDef *timer);
 
-/* global Variables */
+void gpio_setup()
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    LED_PORT->MODER |= GPIO_MODER_MODER5_0;
+}
+
+void timer_setup()
+{
+    // enable clock
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+    // Set Prescaler for TIM3
+    TIM3->PSC = TIM3_PRESCALER;
+
+    // Set Auto Relaod Register
+    TIM3->ARR = TIM3_AUTO_RELOAD_VALUE;
+
+    // enable interrupt
+    TIM3->DIER |= TIM_DIER_UIE;
+
+    // Enable Timer 3 module
+    TIM3->CR1 |= TIM_CR1_CEN;
+
+    // Setup NVIC
+    NVIC_EnableIRQ(TIM3_IRQn);
+}
 
 int main()
 {
     __disable_irq();
-    gpio_enable_port_clock(GPIOA);
-    gpio_set_pin_mode(GPIOA, PIN_5, OUTPUT);
+    gpio_setup();
+    timer_setup();
 
-    timer_init(TIM3, TIM3_PRESCALER, TIM3_AUTO_RELOAD_VALUE, TIM3_INTERUPT_ENABLE);
-    __NVIC_EnableIRQ(TIM3_IRQn);
     __enable_irq();
 
-    while(1)
+    while (1)
     {
         // Do nothing
     }
@@ -37,37 +56,9 @@ int main()
 
 void TIM3_IRQHandler(void)
 {
-    gpio_toggle_pin_state(GPIOA, PIN_5);
-    timer_reset_uif(TIM3);
-}
-
-
-/* Funktions */
-void timer_init(TIM_TypeDef *timer, uint16_t prescaler, uint16_t reload_value, bool interrupt_enable)
-{
-    // Enable clock for TIM3 on APB1 Bus
-    if(TIM3 == timer)
+    if (TIM3->SR & TIM_SR_UIF)
     {
-        RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+        LED_PORT->ODR ^= (1 << LED_PIN);
     }
-    // else ... for other timers.
-
-    // Set Prescaler for TIM3 
-    timer->PSC = prescaler;
-
-    // Set Auto Relaod Register
-    timer->ARR = reload_value;
-
-    // enable interrupt
-    if (interrupt_enable)
-    {
-        timer->DIER |= TIM_DIER_UIE;
-    }
-    // Enable Timer 3 module
-    timer->CR1 |= TIM_CR1_CEN;
-}
-
-void timer_reset_uif(TIM_TypeDef *timer)
-{
-    timer->SR &= ~TIM_SR_UIF;
+    TIM3->SR &= ~TIM_SR_UIF;
 }
